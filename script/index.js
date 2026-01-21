77ba1b62e4ee04654bb46d09fb3ee32f13e8772e
Elshin Vadim <elshinvadim@gmail.com>
2026-01-21 15:25:50 +0100

    Created index.js for scripts

--- script/index.js
+++ script/index.js
@@ -1,4 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const sidebar = document.getElementById("sidebar");
+  const toggleButton = document.getElementById("toggleSidebar");
+  const menu_button = document.getElementById("menu-button");
+
+  toggleButton.addEventListener("click", function () {
+    sidebar.classList.toggle("expanded");
+    menu_button.classList.toggle("rotate");
+  });
+});
+
 function toggleMenu() {
-  const menu = document.getElementById('profileMenu');
-  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+  const menu = document.getElementById("profileMenu");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
 }

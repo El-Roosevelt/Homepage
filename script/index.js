35f7ee0f6c6ba5bc997ac3e07a6d3fbda1dc8992
Lucas <lucterlop@alu.edu.gva.es>
2026-01-23 09:16:52 +0100

    terminado menu desplegable

--- script/index.js
+++ script/index.js
@@ -1,4 +1,13 @@
-function toggleMenu() {
-  const menu = document.getElementById('profileMenu');
+const menu = document.getElementById('profileMenu');
+menu.style.display = 'none';
+function toggleMenu() {  
   menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
+
+document.addEventListener('click', function(event) {
+  const isClickInside = document.getElementById('btnProfile').contains(event.target);
+  if (!isClickInside) {
+    menu.style.display = 'none';
+  }
+});
+

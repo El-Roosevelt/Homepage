b3e09396b7b02b2011c587bb93bd0d8ce4edac7b
Elshin Vadim <elshinvadim@gmail.com>
2026-01-23 11:06:06 +0100

    Fixed unattended merge

--- script/index.js
+++ script/index.js
@@ -1,13 +1,15 @@
-const menu = document.getElementById('profileMenu');
-menu.style.display = 'none';
-function toggleMenu() {  
-  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const sidebar = document.getElementById("sidebar");
+  const toggleButton = document.getElementById("toggleSidebar");
+  const menu_button = document.getElementById("menu-button");
 
-document.addEventListener('click', function(event) {
-  const isClickInside = document.getElementById('btnProfile').contains(event.target);
-  if (!isClickInside) {
-    menu.style.display = 'none';
-  }
+  toggleButton.addEventListener("click", function () {
+    sidebar.classList.toggle("expanded");
+    menu_button.classList.toggle("rotate");
+  });
 });
 
+function toggleMenu() {
+  const menu = document.getElementById("profileMenu");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+}

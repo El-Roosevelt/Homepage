49a9b3a6d03c2b65f19865f92b648939e52d2cd9
Kristina Zhuravleva <kris07081992@gmail.com>
2026-01-23 13:54:14 +0100

    formularui cambios

--- script/index.js
+++ script/index.js
@@ -22,3 +22,21 @@ document.addEventListener('click', function(event) {
     menu.style.display = 'none';
   }
 });
+//formulario-Kristin
+(() => {
+    'use strict'
+
+    // Fetch all forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission if invalid
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
